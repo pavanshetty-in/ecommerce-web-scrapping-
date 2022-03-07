9357263f3af156896f431c5fbc643aa2ff85c022
Pavan S <pavans.shetty.2452@gmail.com>
2022-03-07 23:54:01 +0530

    final

--- seams/spiders/seams_shorts.py
+++ seams/spiders/seams_shorts.py
@@ -1,3 +1,4 @@
+from turtle import title
 import scrapy
 
 
@@ -5,19 +6,29 @@ class SeamsShortsSpider(scrapy.Spider):
     name = 'seams_shorts'
     allowed_domains = ['in.seamsfriendly.com']
     start_urls = ['http://in.seamsfriendly.com/collections/shorts']
+    main_url=['http://in.seamsfriendly.com']
 
     def parse(self, response):
-        products=response.css('.ProductItem')
+        
+        product_url =[self.main_url[0] + url for url in response.css('.ProductItem').xpath('.//div/a/@href').extract()]
+        for product in product_url:
+            yield scrapy.Request(product,callback=self.product_extrat)
        
-        for product in products:
-            title=product.xpath("descendant-or-self::*[@class and contains(concat(' ', normalize-space(@class), ' '), ' ProductItem__Title ')]/descendant-or-self::*/a/text()").get()
-            price=product.xpath("descendant-or-self::*[@class and contains(concat(' ', normalize-space(@class), ' '), ' ProductItem__Price ')]/text()").get()
-            image_urls=product.xpath("descendant-or-self::*[@class and contains(concat(' ', normalize-space(@class), ' '), ' ProductItem__Image ')]/@data-src").getall()
-            # colors=product.xpath("")
+        
+    def product_extrat(self,product):
+        title=product.xpath(".//div[@class='Product__Info ']/div/div/div/h1/text()").extract_first()
+        price =product.xpath(".//div[@class='Product__Info ']/div/div/div/div/div/span/text()").extract_first() 
+        description=product.xpath('.//div[@class="ProductMeta__Description"]/div/p[2]/text()').extract_first()
+        images=product.xpath(".//div[@class='Product__SlideshowNavScroller']/a/img/@src").extract()
+       
+        yield{
+
+            "Title":title.strip(),
+            "Price":price[1:],
+            "Description":description,
             
-            yield{
-                'Title':title,
-                'Price':price[1:],
-                'image_urls':["https:"+img for img in image_urls],
-            }
+            "Images_URLs":[" https:"+image for image in images],
+        }
+
+
             
